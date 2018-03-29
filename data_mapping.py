5e720a8cc0a35bad4f27a9b4beca3229e43e48f4
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-03-29 15:40:00 -0400

    Allow mapping of 2 graphs at once

--- data_mapping.py
+++ data_mapping.py
@@ -5,6 +5,7 @@ Maps graphs data to visual representation.
 # System Imports.
 # from plotly import plotly
 # from plotly.graph_objs import *
+import pylab
 from matplotlib import pyplot
 
 import networkx
@@ -17,35 +18,53 @@ from resources import logging
 logger = logging.get_logger(__name__)
 
 
-class DataMap():
+class DataMapping():
     """
     Data mapping class.
     """
-    def __init__(self, graph):
-        self.nx_graph = networkx.DiGraph()
-        self.backend_graph = graph
+    def __init__(self, graph_1, graph_2):
+        self.nx_graph_1 = networkx.DiGraph()
+        self.nx_graph_2 = networkx.DiGraph()
+        self.backend_graph_1 = graph_1
+        self.backend_graph_2 = graph_2
 
-        self.initialize_graph()
+        self.initialize_graphs()
 
-    def initialize_graph(self):
+    def initialize_graphs(self):
         """
         Initializes visual graph object.
         """
+        # Graph 1.
         # Add all nodes to graph.
-        for key, value in self.backend_graph.nodes.items():
-            self.nx_graph.add_node(self.backend_graph.get_node(key))
+        for key, value in self.backend_graph_1.nodes.items():
+            self.nx_graph_1.add_node(self.backend_graph_1.get_node(key))
 
         # Establish edge connections between nodes.
-        for key, value in self.backend_graph.nodes.items():
+        for key, value in self.backend_graph_1.nodes.items():
             for link in value.edges_out:
-                self.nx_graph.add_edge(self.backend_graph.get_node(key), link)
+                self.nx_graph_1.add_edge(self.backend_graph_1.get_node(key), link)
+
+        # Graph 2.
+        if self.backend_graph_2 is not None:
+            # Add all nodes to graph.
+            for key, value in self.backend_graph_2.nodes.items():
+                self.nx_graph_2.add_node(self.backend_graph_2.get_node(key))
+
+            # Establish edge connections between nodes.
+            for key, value in self.backend_graph_2.nodes.items():
+                for link in value.edges_out:
+                    self.nx_graph_2.add_edge(self.backend_graph_2.get_node(key), link)
+        else:
+            self.nx_graph_2 = None
 
     def draw_map(self):
         """
         Draws current network map.
         """
         pyplot.subplot(121)
-        networkx.draw(self.nx_graph, with_labels=True, font_weight='bold')
-        # pyplot.subplot(122)
+        networkx.draw(self.nx_graph_1, with_labels=True, font_weight='bold')
+        if self.nx_graph_2 is not None:
+            pyplot.subplot(122)
+            networkx.draw_spring(self.nx_graph_2, with_labels=True, font_weight='bold')
         # networkx.draw_shell(test_graph, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
         pyplot.show()
--- main.py
+++ main.py
@@ -18,29 +18,30 @@ logger = logging.get_logger(__name__)
 logger.info('Starting program.')
 
 # Create test graph.
-test_graph = graph.Graph('Test Graph')
-node_0 = test_graph.add_node()
-node_1 = test_graph.add_node(edges_in=[node_0, ])
-node_2 = test_graph.add_node(edges_in=[node_1, ])
-node_3 = test_graph.add_node(edges_in=[node_2, ])
-node_4 = test_graph.add_node(edges_in=[node_3, ], edges_out=[node_0, ])
-logger.info(test_graph.info_string())
+test_graph_1 = graph.Graph('Test Graph')
+node_0 = test_graph_1.add_node()
+node_1 = test_graph_1.add_node(edges_in=[node_0, ])
+node_2 = test_graph_1.add_node(edges_in=[node_1, ])
+node_3 = test_graph_1.add_node(edges_in=[node_2, ])
+node_4 = test_graph_1.add_node(edges_in=[node_3, ], edges_out=[node_0, ])
+logger.info(test_graph_1.info_string())
 
 # Map graph to visual representation.
-mapper = data_mapping.DataMap(test_graph)
+mapper = data_mapping.DataMapping(test_graph_1, None)
 mapper.draw_map()
 
 
 # Reset graph connections.
-test_graph.drop_all_nodes()
+# test_graph.drop_all_nodes()
+test_graph_2 = graph.Graph()
 
 # Draw new connections.
-node_0 = test_graph.add_node()
-node_1 = test_graph.add_node(edges_in=[node_0, ])
-node_2 = test_graph.add_node(edges_in=[node_1, ])
-node_3 = test_graph.add_node(edges_in=[node_1, node_2, ], edges_out=[node_2,])
-node_4 = test_graph.add_node(edges_in=[node_3, ], edges_out=[node_0, node_2, ])
-mapper = data_mapping.DataMap(test_graph)
+node_0 = test_graph_2.add_node()
+node_1 = test_graph_2.add_node(edges_in=[node_0, ])
+node_2 = test_graph_2.add_node(edges_in=[node_1, ])
+node_3 = test_graph_2.add_node(edges_in=[node_1, node_2, ], edges_out=[node_2, ])
+node_4 = test_graph_2.add_node(edges_in=[node_3, ], edges_out=[node_0, node_2, ])
+mapper = data_mapping.DataMapping(test_graph_1, test_graph_2)
 mapper.draw_map()
 
 
